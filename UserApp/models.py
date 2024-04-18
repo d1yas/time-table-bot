84c0d3c743e8d2d83e1aaf63e62304fef24ecaab
diyas08 <newkingg08@gmail.com>
2024-04-18 15:14:14 +0500

    fix

--- UserApp/models.py
+++ UserApp/models.py
@@ -6,10 +6,17 @@ class TeacherModel(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
     patronymic = models.CharField(max_length=20)
-class StudentModel(models.Model):
-    class_name = models.CharField(max_length=5)
-
-
-
     def __str__(self):
         return self.name
+
+
+
+
+
+class StudentModel(models.Model):
+    class_name = models.CharField(max_length=5)
+    def __str__(self):
+        return self.class_name
+
+
+
